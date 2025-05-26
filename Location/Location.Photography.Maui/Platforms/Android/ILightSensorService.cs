0c5b2eb140df958be55f2d3f8c4c64bc11f6dca5
Andrew Winn <brokerandy25@gmail.com>
2025-05-26 16:58:08 -0400

    aa

---
+++ Location/Location.Photography.Maui/Platforms/Android/ILightSensorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Location.Photography.Maui.Platforms.Android
+{
+    public interface ILightSensorService
+    {
+        void StartListening();
+        void StopListening();
+        float GetCurrentLux();
+    }
+}
